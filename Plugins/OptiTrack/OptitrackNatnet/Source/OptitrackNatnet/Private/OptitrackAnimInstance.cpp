1872e8007cbfa0d5c02ff4b6f909c8c5f22835a1
Brian Foster <brianisbrilliant@users.noreply.github.com>
2024-04-13 22:00:26 -0500

    removed optitrack, added retartgeting.

--- Plugins/OptiTrack/OptitrackNatnet/Source/OptitrackNatnet/Private/OptitrackAnimInstance.cpp
+++
@@ -1,9 +0,0 @@
-//======================================================================================================
-// Copyright 2018, NaturalPoint Inc.
-//====================================================================================================== 
-#include "OptitrackAnimInstance.h"
-
-
-UOptitrackAnimInstance::UOptitrackAnimInstance(const FObjectInitializer& ObjectInitializer)
-    : Super(ObjectInitializer)
-{}
